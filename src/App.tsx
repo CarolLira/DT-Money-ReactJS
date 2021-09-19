ca39ad1d83c009980462738df60bf4a4c869a7ee
CarolLira <carol017lira@gmail.com>
2021-09-19 09:31:25 -0300

    :sparkles: Create modal with react-modal library

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,48 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 import { createServer } from 'miragejs';
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 createServer({
-    routes() {
-        this.namespace = 'api';
-        this.get('/transactions', () => {
-          return [
-            {
-              id: 1,
-              title: 'Transaction 1',
-              amount: 400,
-              type: 'deposit',
-              category: 'Food',
-              createdAt: '18/09/2021',
-            }
-          ]
-        });
-    }
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: '18/09/2021',
+        }
+      ];
+    });
+  }
 });
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
-      <Dashboard/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
